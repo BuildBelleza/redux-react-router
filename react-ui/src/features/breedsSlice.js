a784208abd0143b16afb630c8b5be3f3f15745df
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-05-23 19:02:44 -0400

    update

--- react-ui/src/features/breedsSlice.js
+++ react-ui/src/features/breedsSlice.js
@@ -1,12 +1,26 @@
+// eslint-disable-next-line no-unused-vars
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchBreeds = createAsyncThunk('breeds/all', async () => {
+  // eslint-disable-next-line no-unused-vars
+  const response = await fetch('https://dog.ceo/api/breeds/list/all');
+  const data = await response.json();
+  const breeds = Object.keys(data.message);
+  return breeds;
+});
 
 export const breedsSlice = createSlice({
   name: 'breeds',
   initialState: [],
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchBreeds.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  }
 });
 
-// allows us to read from this slice of state
+// allows us to read from this slice of state - very important to understand that
 export const selectBreeds = state => state.breeds;
 
 export default breedsSlice.reducer;
