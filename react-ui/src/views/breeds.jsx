822e7a15726f96788ea2e4bd02a71945dbcaad0d
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-05-23 19:26:55 -0400

    update

--- react-ui/src/views/breeds.jsx
+++ react-ui/src/views/breeds.jsx
@@ -1,14 +1,14 @@
-// eslint-disable-next-line no-unused-vars
+
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
+
 import { useSelector } from 'react-redux';
 
 const Breeds = () => {
-  // eslint-disable-next-line no-unused-vars, no-undef
+  
   const data = useSelector(selectBreeds);
 
   const breeds = data && data.map(breed => {
-   // eslint-disable-next-line no-unused-vars
+  
     return <li>{breed}</li>; 
   });
 
