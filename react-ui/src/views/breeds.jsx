79d651fdc698e9810ef562d0e069a40bc189c01a
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-05-25 13:14:59 -0400

    toggle selected breeds

--- react-ui/src/views/breeds.jsx
+++ react-ui/src/views/breeds.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectBreeds } from '../features/breedsSlice';
 
 const Breeds = () => {
+  const [selectedBreeds, setSelectedBreeds] = useState([]);
 
   const data = useSelector(selectBreeds); // data is our local state memory
 
   const handleClick = e => {
     const value = e.target.dataset.breed;
-  }
+    let _selectedBreeds = [];
+    if (selectedBreeds.indexOf(value) > -1) {
+      const _selectedBreeds = selectedBreeds.filter(breed => breed !== value);
+    } else {
+      _selectedBreeds = [...selectedBreeds, value];
+      setSelectedBreeds(_selectedBreeds);
+    }
+    
+    }
 
-  const breeds = data && data.map(breed => {
-    return <li data-breed={breed} key={breed} onClick={handleClick}>{breed}</li>; 
-  })
+    const breeds = data && data.map(breed => {
+      return <li data-breed={breed} key={breed} onClick={handleClick}>{breed}</li>;
+    })
 
-  return (
-    <>
-      <h1>Breeds</h1>
-      <br></br>
-    <ul>{breeds}</ul>
-</>
-      )
-};
+    return (
+      <>
+        {JSON.stringify(selectedBreeds)}
+        <h1>Breeds</h1>
+        <br></br>
+        <ul>{breeds}</ul>
+        
+      </>
+    )
+  };
 
-export default Breeds;
+  export default Breeds;
